1e246bcdd4d5d3a3bc6697a41e0bc436947ba66d
Nestor Pena <nestor2509@gmail.com>
2024-05-21 10:03:40 -0500

    se ajusta el orden de ejecucion de los plugins

---
+++ app/utils/templates/Plugin.py
@@ -0,0 +1,62 @@
+from app.utils.PluginClass import PluginClass
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.utils import DatabaseHandler
+from flask import request
+from celery import shared_task
+from dotenv import load_dotenv
+import os
+from app.api.records.models import RecordUpdate
+from bson.objectid import ObjectId
+
+load_dotenv()
+
+mongodb = DatabaseHandler.DatabaseHandler()
+
+USER_FILES_PATH = os.environ.get('USER_FILES_PATH', '')
+WEB_FILES_PATH = os.environ.get('WEB_FILES_PATH', '')
+ORIGINAL_FILES_PATH = os.environ.get('ORIGINAL_FILES_PATH', '')
+
+class ExtendedPluginClass(PluginClass):
+    def __init__(self, path, import_name, name, description, version, author, type, settings):
+        super().__init__(path, __file__, import_name, name, description, version, author, type, settings)
+
+    def add_routes(self):
+        @self.route('/bulk', methods=['POST'])
+        @jwt_required()
+        def process_files():
+            current_user = get_jwt_identity()
+            body = request.get_json()
+
+            if 'post_type' not in body:
+                return {'msg': 'No se especificó el tipo de contenido'}, 400
+            
+            if not self.has_role('admin', current_user) and not self.has_role('processing', current_user):
+                return {'msg': 'No tiene permisos suficientes'}, 401
+
+            task = self.bulk.delay(body, current_user)
+            self.add_task_to_user(task.id, 'FUNCNAME', current_user, 'msg')
+            
+            return {'msg': 'Se agregó la tarea a la fila de procesamientos'}, 201
+        
+    @shared_task(ignore_result=False, name='FUNCNAME')
+    def bulk(body, user):
+        filters = {
+            'post_type': body['post_type']
+        }
+
+        if 'parent' in body:
+            if body['parent']:
+                filters = {'$or': [{'parents.id': body['parent'], 'post_type': body['post_type']}, {'_id': ObjectId(body['parent'])}]}
+
+        return 'ok'
+    
+plugin_info = {
+    'name': '',
+    'description': '',
+    'version': '0.1',
+    'author': '',
+    'type': ['bulk'],
+    'settings': {
+        'settings_bulk': []
+    }
+}
