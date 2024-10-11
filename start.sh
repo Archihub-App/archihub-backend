3a9b94610b12b0bc72251b63b8cd9d0d9841571b
Nestor Pena <nestor2509@gmail.com>
2024-10-11 14:00:42 -0500

    ajustes prod

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ cd /app
 if [ "$FLASK_ENV" = "DEV" ]; then
     echo "Running Flask in development mode"
     flask run --host=0.0.0.0
+    sleep 600000
 else if [ "$FLASK_ENV" = "PROD" ]; then
-    gunicorn -w 10 -b 0.0.0.0:${FLASK_RUN_PORT} --certfile=./keys/cert.crt --keyfile=./keys/key.key app:app
+    gunicorn -w 10 -b 0.0.0.0:${FLASK_RUN_PORT} app:app
