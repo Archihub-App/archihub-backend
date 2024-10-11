0213817d718ba6df9453e6f8b1a8748b8b477dcb
Nestor Pena <nestor2509@gmail.com>
2024-10-11 14:07:00 -0500

    removed burned path

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ if [ "$FLASK_ENV" = "DEV" ]; then
     flask run --host=0.0.0.0
     sleep 600000
 elif [ "$FLASK_ENV" = "PROD" ]; then
-    gunicorn -w 10 -b 0.0.0.0:${FLASK_RUN_PORT} --certfile=./keys/cert.crt --keyfile=./keys/key.key app:app
+    gunicorn -w 10 -b 0.0.0.0:${FLASK_RUN_PORT} app:app
 fi
