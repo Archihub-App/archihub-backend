3fc37cb88b7de448daf9800c8f5c1eddb0fabf9b
Nestor Pena <nestor2509@gmail.com>
2024-10-17 19:45:35 -0500

    mejoras en publicAPI para la vista detalle

--- app/api/publicApi/routes.py
+++ app/api/publicApi/routes.py
@@ -8,10 +8,13 @@ import json
 @fernetAuthenticate
 def get_all(username, isAdmin):
     body = request.json
-    body = json.dumps(body)
 
-    from app.api.resources.public_services import get_all
-    resp = get_all(body)
+    if 'keyword' in body and body['keyword'] != '':
+      from app.api.search.public_services import get_resources_by_filters
+      resp = get_resources_by_filters(body)
+    else:
+      from app.api.resources.public_services import get_all
+      resp = get_all(json.dumps(body))
 
     if isinstance(resp, list):
       return tuple(resp)
@@ -24,6 +27,17 @@ def get_types(username, isAdmin):
     from app.api.types.services import get_all as get_all_types
     resp = get_all_types()
 
+    if isinstance(resp, list):
+      return tuple(resp)
+    else:
+      return resp
+
+@bp.route('/resources/<id>', methods=['GET'])
+@fernetAuthenticate
+def get_item(username, isAdmin, id):
+    from app.api.resources.public_services import get_by_id as get_by_id_public
+    resp =  get_by_id_public(id)
+    
     if isinstance(resp, list):
       return tuple(resp)
     else:
