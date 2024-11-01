841ce699d6fddae44e7af4b479fcb3c0c46fb9a5
Nestor Pena <nestor2509@gmail.com>
2024-11-01 11:57:22 -0500

    nueva estructura de carpetas para las tasks

--- app/api/system/services.py
+++ app/api/system/services.py
@@ -25,13 +25,11 @@ from bson.objectid import ObjectId
 
 hookHandler = HookHandler.HookHandler()
 mongodb = DatabaseHandler.DatabaseHandler()
-index_handler = IndexHandler.IndexHandler()
 cacheHandler = CacheHandler.CacheHandler()
 vectorHandler = VectorDatabaseHandler.VectorDatabaseHandler()
 
-ELASTIC_INDEX_PREFIX = os.environ.get('ELASTIC_INDEX_PREFIX', '')
-
 def hookHandlerIndex():
+    from app.api.system.tasks.elasticTasks import index_resources_task, index_resources_delete_task, regenerate_index_task
     hookHandler.register('resource_create', index_resources_task, queue=101)
     hookHandler.register('resource_update', index_resources_task, queue=101)
     hookHandler.register(
@@ -676,101 +674,4 @@ def clear_cache():
         return {'msg': str(e)}, 500
 
 
-@shared_task(ignore_result=False, name='system.regenerate_index')
-def regenerate_index_task(mapping, user):
-    keys = index_handler.get_alias_indexes(
-        ELASTIC_INDEX_PREFIX + '-resources').keys()
-    if len(keys) == 1:
-        name = list(keys)[0]
-        number = name.split('_')[1]
-        number = int(number) + 1
-        new_name = ELASTIC_INDEX_PREFIX + '-resources_' + str(number)
-        index_handler.create_index(
-            new_name, settings=spanish_settings, mapping=mapping)
-        index_handler.add_to_alias(
-            ELASTIC_INDEX_PREFIX + '-resources', new_name)
-        index_handler.reindex(name, new_name)
-        index_handler.remove_from_alias(
-            ELASTIC_INDEX_PREFIX + '-resources', name)
-        index_handler.delete_index(name)
 
-        return 'ok'
-    else:
-        index_handler.start_new_index(mapping)
-        return 'ok'
-
-
-@shared_task(ignore_result=False, name='system.index_resources')
-def index_resources_task(body={}):
-    skip = 0
-    filters = {}
-    loop = True
-    if '_id' in body:
-        filters['_id'] = ObjectId(body['_id'])
-
-    resources = list(mongodb.get_all_records(
-        'resources', filters, limit=1000, skip=skip))
-
-    if body == {}:
-        index_handler.delete_all_documents(ELASTIC_INDEX_PREFIX + '-resources')
-
-    while len(resources) > 0 and loop:
-        for resource in resources:
-            document = {}
-            post_type = resource['post_type']
-            fields = get_metadata(post_type)['fields']
-            for f in fields:
-                if f['type'] != 'file' and f['type'] != 'simple-date':
-                    destiny = f['destiny']
-                    if destiny != '':
-                        value = get_value_by_path(resource, destiny)
-                        if value != None:
-                            document = change_value(
-                                document, f['destiny'], value)
-                elif f['type'] == 'simple-date':
-                    destiny = f['destiny']
-                    if destiny != '':
-                        value = get_value_by_path(resource, destiny)
-                        if value != None:
-                            value = value.strftime('%Y-%m-%d')
-                            change_value(document, f['destiny'], value)
-
-            document['post_type'] = post_type
-            document['parents'] = resource['parents']
-            document['parent'] = resource['parent']
-            document['ident'] = resource['ident']
-            document['status'] = resource['status']
-            document['accessRights'] = 'public'
-            document['files'] = len(
-                resource['filesObj']) if 'filesObj' in resource else 0
-
-            if 'accessRights' in resource:
-                if resource['accessRights']:
-                    document['accessRights'] = resource['accessRights']
-
-            r = index_handler.index_document(
-                ELASTIC_INDEX_PREFIX + '-resources', str(resource['_id']), document)
-            if r.status_code != 201 and r.status_code != 200:
-                raise Exception(
-                    'Error al indexar el recurso ' + str(resource['_id']))
-
-        if len(resources) < 1000:
-            loop = False
-
-        skip += 1000
-        resources = list(mongodb.get_all_records(
-            'resources', {}, limit=1000, skip=skip))
-
-    return 'ok'
-
-
-
-
-@shared_task(ignore_result=False, name='system.index_resources_delete')
-def index_resources_delete_task(body={}):
-    r = index_handler.delete_document(
-        ELASTIC_INDEX_PREFIX + '-resources', body['_id'])
-    if r['result'] != 'deleted':
-        raise Exception('Error al indexar el recurso ' + str(body['_id']))
-
-    return 'ok'
---
+++ app/api/system/tasks/elasticTasks.py
@@ -0,0 +1,103 @@
+from celery import shared_task
+from app.utils import DatabaseHandler
+from app.utils import IndexHandler
+import os
+
+index_handler = IndexHandler.IndexHandler()
+mongodb = DatabaseHandler.DatabaseHandler()
+ELASTIC_INDEX_PREFIX = os.environ.get('ELASTIC_INDEX_PREFIX', '')
+
+@shared_task(ignore_result=False, name='system.regenerate_index')
+def regenerate_index_task(mapping, user):
+    keys = index_handler.get_alias_indexes(
+        ELASTIC_INDEX_PREFIX + '-resources').keys()
+    if len(keys) == 1:
+        name = list(keys)[0]
+        number = name.split('_')[1]
+        number = int(number) + 1
+        new_name = ELASTIC_INDEX_PREFIX + '-resources_' + str(number)
+        index_handler.create_index(
+            new_name, settings=spanish_settings, mapping=mapping)
+        index_handler.add_to_alias(
+            ELASTIC_INDEX_PREFIX + '-resources', new_name)
+        index_handler.reindex(name, new_name)
+        index_handler.remove_from_alias(
+            ELASTIC_INDEX_PREFIX + '-resources', name)
+        index_handler.delete_index(name)
+
+        return 'ok'
+    else:
+        index_handler.start_new_index(mapping)
+        return 'ok'
+
+@shared_task(ignore_result=False, name='system.index_resources')
+def index_resources_task(body={}):
+    skip = 0
+    filters = {}
+    loop = True
+    if '_id' in body:
+        filters['_id'] = ObjectId(body['_id'])
+
+    resources = list(mongodb.get_all_records(
+        'resources', filters, limit=1000, skip=skip))
+
+    if body == {}:
+        index_handler.delete_all_documents(ELASTIC_INDEX_PREFIX + '-resources')
+
+    while len(resources) > 0 and loop:
+        for resource in resources:
+            document = {}
+            post_type = resource['post_type']
+            fields = get_metadata(post_type)['fields']
+            for f in fields:
+                if f['type'] != 'file' and f['type'] != 'simple-date':
+                    destiny = f['destiny']
+                    if destiny != '':
+                        value = get_value_by_path(resource, destiny)
+                        if value != None:
+                            document = change_value(
+                                document, f['destiny'], value)
+                elif f['type'] == 'simple-date':
+                    destiny = f['destiny']
+                    if destiny != '':
+                        value = get_value_by_path(resource, destiny)
+                        if value != None:
+                            value = value.strftime('%Y-%m-%d')
+                            change_value(document, f['destiny'], value)
+
+            document['post_type'] = post_type
+            document['parents'] = resource['parents']
+            document['parent'] = resource['parent']
+            document['ident'] = resource['ident']
+            document['status'] = resource['status']
+            document['accessRights'] = 'public'
+            document['files'] = len(
+                resource['filesObj']) if 'filesObj' in resource else 0
+
+            if 'accessRights' in resource:
+                if resource['accessRights']:
+                    document['accessRights'] = resource['accessRights']
+
+            r = index_handler.index_document(
+                ELASTIC_INDEX_PREFIX + '-resources', str(resource['_id']), document)
+            if r.status_code != 201 and r.status_code != 200:
+                raise Exception(
+                    'Error al indexar el recurso ' + str(resource['_id']))
+
+        if len(resources) < 1000:
+            loop = False
+
+        skip += 1000
+        resources = list(mongodb.get_all_records(
+            'resources', {}, limit=1000, skip=skip))
+
+    return 'ok'
+
+@shared_task(ignore_result=False, name='system.index_resources_delete')
+def index_resources_delete_task(body={}):
+    r = index_handler.delete_document(
+        ELASTIC_INDEX_PREFIX + '-resources', body['_id'])
+    if r['result'] != 'deleted':
+        raise Exception('Error al indexar el recurso ' + str(body['_id']))
+
+    return 'ok'
