75e5d72e6f52da69966cdccfdd59bac913a3887b
PedroRuizCode <pedroruizzarate@gmail.com>
2025-05-01 09:30:52 -0500

    Endpoint to see changes in resources

--- app/api/logs/services.py
+++ app/api/logs/services.py
@@ -51,6 +51,81 @@ def filter(body):
         return logs, 200
     except Exception as e:
         return {'msg': str(e)}, 500
+
+# Comparar dos objetos y obtener los cambios
+def compare_objects(old_obj, new_obj, path_prefix, date):
+    changes = []
+    
+    # Obtener todas las claves de ambos objetos
+    all_keys = set(list(old_obj.keys()) + list(new_obj.keys()))
+    print(all_keys)
+    
+    for key in all_keys:
+        current_path = f"{path_prefix}.{key}" if path_prefix else key
+        print(current_path)
+        
+        # Key neuvo (solo está en el nuevo objeto)
+        if key not in old_obj:
+            changes.append({
+                "path": current_path,
+                "date": date,
+                "old": None,
+                "new": new_obj[key]
+            })
+            continue
+            
+        # Key eliminado (no está en el nuevo objeto)
+        if key not in new_obj:
+            changes.append({
+                "path": current_path,
+                "date": date,
+                "old": old_obj[key],
+                "new": None
+            })
+            continue
+        
+        old_val = old_obj[key]
+        new_val = new_obj[key]
+        
+        if old_val != new_val:
+            # Si los valores son diccionarios, comparar recursivamente
+            if isinstance(old_val, dict) and isinstance(new_val, dict):
+                nested_changes = compare_objects(old_val, new_val, current_path, date)
+                changes.extend(nested_changes)
+            # Si no son iguales y no son diccionarios, agregar el cambio
+            else:
+                changes.append({
+                    "path": current_path,
+                    "date": date,
+                    "old": old_val,
+                    "new": new_val
+                })
+    
+    return changes
+
+def extract_changes(logs):
+    changes = []
+    print(logs)
+    # Revisar que haya al menos dos logs para comparar
+    if len(logs) < 2:
+        print("No hay suficientes logs para comparar.")
+        return changes
+    
+    # Comparar pares de logs consecutivos
+    for i in range(len(logs) - 1):
+        current_log = logs[i]
+        next_log = logs[i + 1]
+        
+        # Obtener los objetos de los logs
+        current_resource = current_log.get('metadata', {}).get('resource', {}).get('metadata', {})
+        next_resource = next_log.get('metadata', {}).get('resource', {}).get('metadata', {})
+        
+        # Comparar los objetos y obtener los cambios con la fecha del siguiente log
+        date = next_log['date']['$date']
+        detected_changes = compare_objects(current_resource, next_resource, "", date)
+        changes.extend(detected_changes)
+    
+    return changes
     
 # Funcion para obtener el total de recursos
 @cacheHandler.cache.cache()
@@ -64,3 +139,28 @@ def get_total(obj):
         return total
     except Exception as e:
         raise Exception(str(e))
+    
+# Funcion para cambios en los logs de un recurso
+def get_logs(resource_id):
+    try:
+        # Obtener todos los logs de la coleccion logs
+        logs = mongodb.get_all_records('logs',
+                                      {'metadata.resource._id': resource_id,
+                                       'action': {'$in': ["RESOURCE_CREATE", "RESOURCE_UPDATE"]}},
+                                      limit=20,
+                                      sort=[('date', 1)],
+                                      fields={'_id': 0})
+        # Si no hay logs, retornar error
+        if not logs:
+            return {'msg': _('Logs not found')}, 404
+        
+        # Parsear el resultado
+        logs = parse_result(logs)
+
+        # Extraer los cambios de los logs
+        changes = extract_changes(logs)
+
+        # Retornar el resultado
+        return changes, 200
+    except Exception as e:
+        return {'msg': str(e)}, 500
