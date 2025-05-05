1028dbce2b37250c84411f437373b11282e2c6bf
Nestor Pena <nestor2509@gmail.com>
2025-05-05 15:18:42 -0500

    avances

--- app/plugins/liquidText/__init__.py
+++ app/plugins/liquidText/__init__.py
@@ -3,6 +3,7 @@ from app.utils.PluginClass import PluginClass
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.utils import DatabaseHandler
 from flask import request, send_file
+from flask_babel import _
 from celery import shared_task
 from dotenv import load_dotenv
 import os
@@ -10,6 +11,7 @@ from app.api.resources.models import ResourceUpdate
 from bson.objectid import ObjectId
 import pandas as pd
 import datetime
+import shutil
 
 load_dotenv()
 
@@ -18,7 +20,7 @@ mongodb = DatabaseHandler.DatabaseHandler()
 USER_FILES_PATH = os.environ.get('USER_FILES_PATH', '')
 WEB_FILES_PATH = os.environ.get('WEB_FILES_PATH', '')
 ORIGINAL_FILES_PATH = os.environ.get('ORIGINAL_FILES_PATH', '')
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
+TEMPORAL_FILES_PATH = os.environ.get('TEMPORAL_FILES_PATH', '')
 
 class ExtendedPluginClass(PluginClass):
     def __init__(self, path, import_name, name, description, version, author, type, settings, actions, capabilities=None):
@@ -38,6 +40,197 @@ class ExtendedPluginClass(PluginClass):
             self.add_task_to_user(task.id, 'liquidText.generateLiquidText', current_user, 'msg')
             
             return {'msg': 'Se agregó la tarea a la fila de procesamientos'}, 201
+        
+        @self.route('/filedownload/<taskId>', methods=['GET'])
+        @jwt_required()
+        def file_download(taskId):
+            current_user = get_jwt_identity()
+
+            if not self.has_role('admin', current_user) and not self.has_role('processing', current_user) and not self.has_role('editor', current_user):
+                return {'msg': 'No tiene permisos suficientes'}, 401
+            
+            # Buscar la tarea en la base de datos
+            task = mongodb.get_record('tasks', {'taskId': taskId})
+            # Si la tarea no existe, retornar error
+            if not task:
+                return {'msg': 'Tarea no existe'}, 404
+            
+            if task['user'] != current_user and not self.has_role('admin', current_user):
+                return {'msg': 'No tiene permisos para obtener la tarea'}, 401
+
+            if task['status'] == 'pending':
+                return {'msg': 'Tarea en proceso'}, 400
+
+            if task['status'] == 'failed':
+                return {'msg': 'Tarea fallida'}, 400
+
+            if task['status'] == 'completed':
+                if task['resultType'] != 'file_download':
+                    return {'msg': 'Tarea no es de tipo file_download'}, 400
+                
+            path = USER_FILES_PATH + task['result']
+            response = send_file(path, as_attachment=True, download_name=os.path.basename(path), conditional=False)
+            
+            response.headers.add("Access-Control-Expose-Headers", "Content-Disposition")
+            return response
+        
+        @self.route('/download', methods=['POST'])
+        @jwt_required()
+        def download_liquid_text():
+            current_user = get_jwt_identity()
+            body = request.get_json()
+            
+            if not self.has_role('admin', current_user) and not self.has_role('processing', current_user):
+                return {'msg': 'No tiene permisos suficientes'}, 401
+
+            task = self.download.delay(body, current_user)
+            self.add_task_to_user(task.id, 'liquidText.downloadLiquidText', current_user, 'file_download')
+            
+            return {'msg': 'Se agregó la tarea a la fila de procesamientos'}, 201
+        
+        @self.route('/save', methods=['POST'])
+        @jwt_required()
+        def save_liquid_text():
+            current_user = get_jwt_identity()
+            body = request.get_json()
+            
+            if 'id' not in body:
+                return {'msg': 'No se ha enviado el id del recurso'}, 400
+            
+            if not self.has_role('admin', current_user) and not self.has_role('processing', current_user):
+                return {'msg': 'No tiene permisos suficientes'}, 401
+
+            resp = self.save(body, current_user)
+            
+            return {'msg': resp}, 201
+        
+    def save(self, body, user):
+        instance = ExtendedPluginClass('liquidText', '', **plugin_info)
+        
+        record = mongodb.get_record('records', {'_id': ObjectId(body['id'])}, fields={'_id': 1, 'processing': 1, 'name': 1})
+        if not record:
+            return {'msg': 'No se ha encontrado el recurso'}, 404
+        
+        if 'processing' not in record:
+            return {'msg': 'No se ha encontrado el recurso'}, 404
+        
+        if body['slug'] not in record['processing']:
+            return {'msg': 'No se ha encontrado el recurso'}, 404
+        
+        update = {
+            'processing': record['processing']
+        }
+        
+        update['processing']['liquidText'] = {
+            'type': 'liquidText',
+            'result': {
+                'text': body['text'],
+                'status': 'completed',
+                'date': datetime.datetime.now()
+            }
+        }
+        instance.update_data('records', str(record['_id']), update)
+        
+        from app.utils.functions import cache_get_processing_result
+        cache_get_processing_result.invalidate_all()
+        
+        resp = _("Se ha guardado el texto líquido en el recurso %(resource)s", resource=record['name'])
+        return resp
+        
+    @shared_task(ignore_result=False, name='liquidText.downloadLiquidText')
+    def download(body, user):
+        records_filters = {'_id': {'$in': [ObjectId(record) for record in body['records']]}}
+        
+        records = list(mongodb.get_all_records('records', records_filters, fields={'_id': 1, 'mime': 1, 'filepath': 1, 'processing': 1, 'name': 1, 'displayName': 1}))
+        
+        if len(records) == 0:
+            raise Exception('No se encontraron registros')
+        elif len(records) > 1:
+            raise Exception('Debe seleccionar solo un registro')
+        
+        folder_path = USER_FILES_PATH + '/' + user + '/liquidText'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            
+        def doc_processing(result, path):
+            
+            from docx import Document
+            from bs4 import BeautifulSoup
+            import re
+
+            doc = Document()
+            title = r['displayName'] if 'displayName' in r else r['name']
+            doc.add_heading(title, 0)
+
+            soup = BeautifulSoup(result, 'html.parser')
+
+            for tag in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+                if tag.name == 'p':
+                    paragraph = doc.add_paragraph()
+                    for content in tag.contents:
+                        if content.name in ['b', 'strong']:
+                            run = paragraph.add_run(content.get_text())
+                            run.bold = True
+                        elif content.name in ['i', 'em']:
+                            run = paragraph.add_run(content.get_text())
+                            run.italic = True
+                        elif content.name == 'u':
+                            run = paragraph.add_run(content.get_text())
+                            run.underline = True
+                        elif content.name is None:
+                            paragraph.add_run(str(content))
+                elif re.match(r'h[1-6]', tag.name):
+                    level = int(tag.name[1])
+                    heading = doc.add_heading(level=level)
+                    for content in tag.contents:
+                        if content.name in ['b', 'strong']:
+                            run = heading.add_run(content.get_text())
+                            run.bold = True
+                        elif content.name in ['i', 'em']:
+                            run = heading.add_run(content.get_text())
+                            run.italic = True
+                        elif content.name == 'u':
+                            run = heading.add_run(content.get_text())
+                            run.underline = True
+                        elif content.name is None:
+                            heading.add_run(str(content))
+                    
+            doc.save(path)
+            return path
+        
+        for r in records:
+            if 'liquidText' in r['processing']:
+                result = r['processing']['liquidText']['result']['text']
+                
+                if body['format'] == 'doc':
+                    path = os.path.join(folder_path, str(r['_id']) + '.docx')
+                    doc_processing(result, path)
+                    return '/' + user + '/liquidText/' + str(r['_id']) + '.docx'
+                    
+                elif body['format'] == 'pdf':
+                    temp_path = os.path.join(TEMPORAL_FILES_PATH, str(r['_id']) + '.docx')
+                    doc_processing(result, temp_path)
+                    
+                    try:
+                        from app.plugins.filesProcessing.utils.DocumentProcessing import convert_to_pdf_with_libreoffice
+                    except Exception as e:
+                        raise Exception('Error al importar el módulo del plugin para el procesamiento de documentos: ' + str(e))
+                    
+                    output_pdf = os.path.join(USER_FILES_PATH, user, 'liquidText', str(r['_id']) + '.pdf')
+                    convert_to_pdf_with_libreoffice(temp_path, output_pdf)
+                    shutil.move(os.path.join(TEMPORAL_FILES_PATH, str(r['_id']) + '.pdf'), output_pdf)
+                    os.remove(temp_path)
+                    return '/' + user + '/liquidText/' + str(r['_id']) + '.pdf'
+
+                elif body['format'] == 'txt':
+                    path = os.path.join(folder_path, str(r['_id']) + '.txt')
+                    with open(path, 'w') as f:
+                        f.write(result)
+                    return '/' + user + '/liquidText/' + str(r['_id']) + '.txt'
+                else:
+                    raise Exception('Formato no soportado')
+
+        return 'ok'
         
     @shared_task(ignore_result=False, name='liquidText.generateLiquidText')
     def bulk(body, user):
@@ -125,6 +318,28 @@ plugin_info = {
                     'label': 'Sobreescribir procesamientos existentes',
                     'id': 'overwrite',
                     'default': False,
+                    'required': False,
+                }
+            ]
+        },
+        {
+            'placement': 'detail_record',
+            'record_type': ['audio', 'video'],
+            'label': 'Descargar texto líquido',
+            'roles': ['admin', 'processing', 'editor'],
+            'endpoint': 'download',
+            'icon': 'Download, WaterDrop',
+            'extraOpts': [
+                {
+                    'type': 'select',
+                    'label': 'Formato del archivo',
+                    'id': 'format',
+                    'default': 'pdf',
+                    'options': [
+                        {'value': 'pdf', 'label': 'PDF'},
+                        {'value': 'doc', 'label': 'DOC'},
+                        {'value': 'txt', 'label': 'TXT'},
+                    ],
                     'required': False,
                 }
             ]
--- app/utils/functions.py
+++ app/utils/functions.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 from PIL import Image
 from flask import Response, jsonify
+import datetime
 import base64
 load_dotenv()
 
@@ -32,6 +33,7 @@ def clear_cache():
     cache_get_imgs_gallery_by_id.invalidate_all()
     cache_type_roles.invalidate_all()
     cache_get_processing_metadata.invalidate_all()
+    cache_get_processing_result.invalidate_all()
     has_right.invalidate_all()
     has_role.invalidate_all()
 
@@ -351,7 +353,17 @@ def cache_get_processing_result(id, slug):
     if slug not in record['processing']:
         raise Exception('Record no ha sido procesado con el slug ' + slug)
 
-    return record['processing'][slug]['result']
+    result = record['processing'][slug]['result']
+    # iterate each key in result and if the value is a datetime, convert it to string
+    for key in result:
+        if isinstance(result[key], datetime.datetime):
+            result[key] = result[key].strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(result[key], list):
+            for i in range(len(result[key])):
+                if isinstance(result[key][i], datetime.datetime):
+                    result[key][i] = result[key][i].strftime('%Y-%m-%d %H:%M:%S')
+
+    return result
 
 @cacheHandler.cache.cache(limit=1000)
 def cache_get_processing_metadata(id, slug):
