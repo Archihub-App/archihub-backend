29fba9acf4a3aee605adf3101b0d322299340d04
Nestor Pena <nestor2509@gmail.com>
2025-05-09 11:02:24 -0500

    permisos liquidtext

--- app/api/search/services.py
+++ app/api/search/services.py
@@ -1,7 +1,6 @@
 from flask import jsonify, request
 from app.utils.functions import get_access_rights
 from app.api.system.services import get_system_settings
-import os
 from flask_babel import _
 
 
@@ -9,12 +8,15 @@ def get_resources_by_filters(body, user):
     try:
         capabilities = get_system_settings()
         capabilities = capabilities['capabilities']
-        response = {}
+        response = None
         
         if 'indexing' in capabilities:
             from .utils import elasticUtils
             response = elasticUtils.get_resources_by_filters(body, user)
 
-        return jsonify(response), 200
+        if response:
+            return jsonify(response), 200
+        else:
+            raise Exception(_('No response from the indexing system'))
     except Exception as e:
         return {'msg': str(e)}, 500
