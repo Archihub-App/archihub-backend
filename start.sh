25fc03019aebd18fb771e6ffd142de802070e811
PedroRuizCode <pedroruizzarate@gmail.com>
2025-07-21 11:44:36 -0500

    HOTFIX: Added elastic validations with certs

--- start.sh
+++ start.sh
@@ -5,10 +5,21 @@ cd /app
 # -------- WAIT FOR ELASTICSEARCH --------
 echo "Waiting for Elasticsearch to be healthy..."
 
-until curl -s -f "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s"; do
-  >&2 echo "Elasticsearch is unavailable - sleeping"
-  sleep 5
-done
+if [ -n "$ELASTIC_CERT" ]; then
+  echo "Using certificate authentication for Elasticsearch"
+  
+  until curl -s -f -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s" --cacert "$ELASTIC_CERT"; do
+    >&2 echo "Elasticsearch is unavailable - sleeping"
+    sleep 5
+  done
+else
+  echo "Using standard authentication for Elasticsearch"
+  
+  until curl -s -f -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s"; do
+    >&2 echo "Elasticsearch is unavailable - sleeping"
+    sleep 5
+  done
+fi
 
 # -------- START BACKEND --------
 echo "Elasticsearch is up!"
