3d10f1298631c2b52995ac93926be6e859235b8e
Nestor Pena <nestor2509@gmail.com>
2026-02-17 05:47:19 -0500

    ajustes

--- app/api/resources/services.py
+++ app/api/resources/services.py
@@ -1369,6 +1369,7 @@ def get_resource(id, user, postQuery = False):
                     favorite = mongodb.get_record(favorite_source, {'_id': ObjectId(favorite_id)}, fields=fields)
                     
                     articleBody = favorite['articleBody'] if 'articleBody' in favorite else []
+                    contentArticle = ''
                     for p in articleBody:
                         if 'type' in p and p['type'] == 'paragraph':
                             if 'content' in p:
@@ -1377,15 +1378,16 @@ def get_resource(id, user, postQuery = False):
                                 
 
                                 content = strip_html(p['content'])
-                                if favorite['articleBody'] == '':
-                                    favorite['articleBody'] += content
+                                if contentArticle == '':
+                                    contentArticle += content
                                 else:
-                                    favorite['articleBody'] += ' ' + content
+                                    contentArticle += ' ' + content
                                     
-                                    if len(favorite['articleBody']) > 300:
-                                        favorite['articleBody'] = favorite['articleBody'][:300]
-                                        break
+                                if len(contentArticle) > 300:
+                                    contentArticle = contentArticle[:300]
+                                    break
                     
+                    favorite['articleBody'] = contentArticle
                     files = favorite['filesObj'] if 'filesObj' in favorite else None
                     imagesFiles = []
                     imagePath = None
