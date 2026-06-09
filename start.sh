b78651f45cc2452eb664f099bd8f6f9d8d9cf104
nestorandrespe <nestor2509@gmail.com>
2026-06-09 08:42:10 -0500

    ajustes despliegue

--- start.sh
+++ start.sh
@@ -28,10 +28,10 @@ trap stop_child TERM INT
 # -------- WAIT FOR ELASTICSEARCH --------
 echo "Waiting for Elasticsearch to be healthy..."
 
-if [ -n "$ELASTIC_CERT" ]; then
+if [ -n "${ELASTIC_CERT:-}" ]; then
   echo "Using certificate authentication for Elasticsearch"
   
-  until curl -s -f -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s" --cacert "$ELASTIC_CERT"; do
+  until curl -s -f -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s" --cacert "${ELASTIC_CERT}"; do
     >&2 echo "Elasticsearch is unavailable - sleeping"
     sleep 5
   done
