d3ef8cd9f5b112502e0778f6f15198f19087f5ce
nestorandrespe <nestor2509@gmail.com>
2026-06-16 11:43:38 -0500

    mejoras elastic cert

--- start_celery.sh
+++ start_celery.sh
@@ -28,7 +28,7 @@ trap stop_child TERM INT
 # -------- WAIT FOR ELASTICSEARCH --------
 echo "Waiting for Elasticsearch to be healthy..."
 
-if [ -n "$ELASTIC_CERT" ]; then
+if [ -n "${ELASTIC_CERT:-}" ]; then
   echo "Using certificate authentication for Elasticsearch"
   
   until curl -s -f -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTIC_DOMAIN}:${ELASTIC_PORT}/_cluster/health?wait_for_status=yellow&timeout=1s" --cacert "$ELASTIC_CERT"; do
